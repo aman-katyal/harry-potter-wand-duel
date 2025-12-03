3cb3da229baec02df298e872b9a0b32b753a02fc
rhea-virk <rheavirk@gmail.com>
2025-12-03 18:10:30 -0500

    added comments

--- final_code/lib/ir_transmitter/ir_emitter.h
+++ final_code/lib/ir_transmitter/ir_emitter.h
@@ -4,34 +4,29 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-// === NEC Timing (microseconds) ===
-#define NEC_UNIT       560
-#define NEC_HDR_MARK   (16 * NEC_UNIT)
-#define NEC_HDR_SPACE  (8  * NEC_UNIT)
-#define NEC_BIT_MARK   NEC_UNIT
-#define NEC_ONE_SPACE  (3 * NEC_UNIT)
+//def constants for fsm:
+#define NEC_UNIT 560
+#define NEC_HDR_MARK (16 * NEC_UNIT)
+#define NEC_HDR_SPACE (8  * NEC_UNIT)
+#define NEC_BIT_MARK NEC_UNIT
+#define NEC_ONE_SPACE (3 * NEC_UNIT)
 #define NEC_ZERO_SPACE NEC_UNIT
 #define NEC_STOP_BIT   NEC_UNIT
-#define NEC_GAP_MS     110
+#define NEC_GAP_MS 110
 
-/**
- * Initialize the IR emitter on the specified GPIO pin
- * @param tx_pin GPIO pin number for IR LED (must support PWM)
- */
+//1. Initialize the IR emitter on the specified GPIO pin
+    //tx_pin  = GPIO pin # for IR LED
 void ir_emitter_init(uint32_t tx_pin);
 
-/**
- * Start sending a packet with specified repeats
- * @param addr NEC address value
- * @param cmd NEC command value
- * @param repeats Number of times to send this packet
- */
+
+//2. Start sending a packet with specified repeats
+    //addr = NEC addr val
+    //cmd = NEC cmd val
+    //repeats = # times to send data packet
 void ir_emitter_start(uint8_t addr, uint8_t cmd, uint8_t repeats);
 
-/**
- * Update function - call this repeatedly from main loop
- * Handles all timing and packet transmission automatically
- */
+
+//3. Update function to send mult frames based on given params
 void ir_emitter_update(void);
 
 /**
@@ -40,4 +35,4 @@ void ir_emitter_update(void);
  */
 bool ir_emitter_done(void);
 
-#endif // IR_EMITTER_H
+#endif
